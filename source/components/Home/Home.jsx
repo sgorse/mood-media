6e49c562042b10a04bdf89bbbc6710678143432c
sgorse <sgorse2@illinois.edu>
2018-04-14 05:04:48 -0500

    Just saving, not in anywhere near a working state.

--- source/components/Home/Home.jsx
+++ source/components/Home/Home.jsx
@@ -3,16 +3,69 @@ import { Button } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 
 import styles from './Home.scss'
+import Spotify from 'spotify-web-api-js'
 
+const spotifyWebApi = new Spotify();
 class Home extends Component {
+  constructor(props) {
+    super(props)
+    const params = this.getHashParams()
 
-    render() {
-        return(
-            <div className="Home">
-                <h1>Welcome to MP2!</h1>
-            </div>
-        )
+    this.state = {
+      loggedIn: params.access_token ? true : false,
+      nowPlaying: {
+        name: 'Not Checked',
+        image: ''
+
+      }
     }
+    if(params.access_token) {
+      var myStorage = window.localStorage
+      localStorage.setItem('access_token', params.access_token)
+      localStorage.setItem('logged_In', true)
+      spotifyWebApi.setAccessToken(params.access_token)
+      this.props.set_access_token(params.access_token)
+      this.props.set_loggedIn(true)
+    }
+  }
+  getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+    while ( e = r.exec(q)) {
+       hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+    return hashParams;
+  }
+  getNowPlaying() {
+    spotifyWebApi.getMyCurrentPlaybackState().
+      then((response) => {
+        this.setState({
+          nowPlaying: {
+            name: response.item.name,
+            image: response.item.album.images[0].url
+          }
+        })
+      })
+  }
+  render() {
+      return(
+          <div className="Home">
+            <h1 id="mainTitle">What do you want to consume?</h1>
+            <div className="LinkButtons">
+                <Button id="musicButton" as={Link} to='music' onClick={() => {window.location.reload();}}>Music</Button>
+                <Button id="moviesButton" as={Link} to='movies' onClick={() => {window.location.reload();}}>Movies</Button>
+            </div>
+            <div>Now Playing : {this.state.nowPlaying.name}</div>
+            <div>
+              <img src={ this.state.nowPlaying.image} style={{width: 100}}/>
+            </div>
+            <button onClick={() => this.getNowPlaying()} >
+              Check Now Playing
+            </button>
+          </div>
+      )
+  }
 }
 
 export default Home
--- source/index.jsx
+++ source/index.jsx
@@ -1,18 +1,52 @@
-import React from 'react';
-import {render} from 'react-dom';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
-import 'semantic-ui-css/semantic.min.css';
+import React from 'react'
+import {render} from 'react-dom'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import 'semantic-ui-css/semantic.min.css'
+import { Button } from 'semantic-ui-react'
 
 // Include your new Components here
-import Home from './components/Home/Home.jsx';
+import HomeView from './components/Home/Home.jsx';
+import MusicView from './components/Music/Music.jsx';
+import MoviesView from './components/Movies/Movies.jsx';
 
 // Include any new stylesheets here
 // Note that components' stylesheets should NOT be included here.
 // They should be 'require'd in their component class file.
-require('./styles/main.scss');
+require('./styles/main.scss')
+var access_token = ''
+var loggedIn = false
+let set_access_token = (new_access_token) => {
+  access_token = new_access_token
+}
+let set_loggedIn = (isLoggedIn) => {
+  loggedIn = isLoggedIn
+}
+let get_loggedIn = () => {
+  return loggedIn
+}
+let get_access_token = () => {
+  return access_token
+}
+
+const AllViews = () => (
+  <main>
+    <h4>{access_token}</h4>
+    <Router>
+      <div>
+        <Route exact path="/" render={props => (<HomeView {...props} set_access_token={set_access_token} set_loggedIn={set_loggedIn}/>
+        )}/>
+        <Route path="/music" render={props => (<MusicView {...props} get_access_token={get_access_token} get_loggedIn={get_loggedIn}/> )}/>
+        <Route path="/movies" component={MoviesView}/>
+      </div>
+    </Router>
+  </main>
+)
+
+
 
 render(
-    <Home />,
+
+    <AllViews />,
     // Define your router and replace <Home /> with it!
     document.getElementById('app')
 );
